d86ebdf4c6d473fa593edbd59659043e89a9d4ee
Jonochi <tana.jayne@gmail.com>
2023-03-19 13:03:27 -0600

    Passing properties and successfully rendering file

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const inquirer = require('inquirer');
+const fs = require('fs');
+const { Circle, Triangle, Square, ShapeProperties }  = require('./lib/shapes.js');
 
 const prompts = [
     {
@@ -7,15 +9,58 @@ const prompts = [
         message: 'Enter up to 3 characters for your SVG text'
     },
     {
-        input: 'list',
+        type: 'list',
         name: 'shape',
         message: 'What shape would you like?',
-        choices: ['Square', 'Triangle', 'Circle']
-    }
+        choices: ['Circle', 'Triangle', 'Square']
+    },
+    {
+        type: 'list',
+        name: 'shapeColor',
+        message: 'What color would you like your shape?',
+        choices: ['blue', 'black', 'green']
+    },
+    {
+        type: 'list',
+        name: 'textColor',
+        message: 'What color would you like your text?',
+        choices: ['pink', 'orange', 'white']
+    },
+
 
 ]
 
-inquirer.prompt(prompts)
-.then((data) => {
-    console.dir(data)
-})
+function init() {
+
+
+    inquirer
+        .prompt(prompts)
+        .then((data) => {
+            let formattedText = (data.text).toUpperCase();
+            if (formattedText.length > 3) {
+                formattedText = formattedText.substring(0, 3);
+            }
+
+            let shapeInfo = new ShapeProperties(data.shapeColor, formattedText, data.textColor);
+            console.dir(shapeInfo);
+            let shape;
+            if (data.shape === "Square") {
+                shape = new Square(shapeInfo);
+            } else if (data.shape === "Circle") {
+                shape = new Circle(shapeInfo);
+            } else if (data.shape === "Triangle") {
+                shape = new Triangle(shapeInfo);
+            }
+
+            console.dir(shape);
+            fs.writeFile(`./examples/${data.shape}.svg`, shape.render() , () => {
+                console.log('Success!');
+            })
+            // console.dir(data)
+            // console.dir(limitChars)
+            // console.dir(shape)
+        })
+    
+}
+
+init();
